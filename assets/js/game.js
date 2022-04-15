01a14d2d759db2888b6e61b81aa64b72db60062d
Brandon Benson <brandonbenson09@gmail.com>
2022-04-14 22:07:32 -0400

    added shop change

--- assets/js/game.js
+++ assets/js/game.js
@@ -220,22 +220,20 @@ var endGame = function () {
 var shop = function () {
   //ask player what theyd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1, 2, or 3 for LEAVE."
   );
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   // use switch to carry out action
   switch (shopOptionPrompt) {
-    case "refill":
-    case "REFILL":
+    case 1:
       playerInfo.refillHealth();
       break;
 
-    case "upgrade":
-    case "UPGRADE":
+    case 2:
       playerInfo.upgradeAttack();
       break;
 
-    case "leave":
-    case "LEAVE":
+    case 3:
       window.alert("Leaving the store.");
       // do nothing, so function will end
       break;
